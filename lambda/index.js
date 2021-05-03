df1f974b58479402cde6dc5b959663d143f9cc06
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-05-03 16:09:09 -0700

    Enable plus sign.

--- lambda/index.js
+++ lambda/index.js
@@ -3,6 +3,7 @@ var LambdaForwarder = require("aws-lambda-ses-forwarder");
 exports.handler = function(event, context, callback) {
     var overrides = {
         config: {
+            allowPlusSign: true,
             fromEmail: process.env.EMAIL_FROM,
             emailBucket: process.env.EMAIL_BUCKET_NAME,
             emailKeyPrefix: process.env.EMAIL_BUCKET_PATH,
